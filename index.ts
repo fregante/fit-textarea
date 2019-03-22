2723ecf936fc60310a5bf240d0296666b4bf942c
Federico Brigante <github@bfred.it>
2019-03-22 14:16:29 +0800

    Shorten

--- index.ts
+++ index.ts
@@ -1,31 +1,23 @@
-type Positions = Map<Element, number>;
-
-function fitTextarea(element: HTMLTextAreaElement): void {
-	const positions = getScrollPositions(element);
-	element.style.height = 'auto';
-	const style = getComputedStyle(element);
-
-	element.style.height = String(element.scrollHeight + parseFloat(style.borderTopWidth!) + parseFloat(style.borderBottomWidth!)) + 'px';
-	setScrollPositions(positions);
-}
-
-function getScrollPositions(element: Element): Positions {
-	const positions: Positions = new Map();
-
+function fitTextarea(textarea: HTMLTextAreaElement): void {
+	// Resetting the height will change the page height and might change the scroll
+	const positions: Map<Element, number> = new Map();
+	let element: Element = textarea;
 	while (element && element.parentElement) {
-		if (element.parentElement.scrollTop) {
-			positions.set(element.parentElement, element.parentElement.scrollTop);
-		}
-
 		element = element.parentElement;
+		positions.set(element, element.scrollTop);
 	}
 
-	return positions;
-}
+	// Reset the height to get the smallest possible height
+	textarea.style.height = 'auto';
+	const style = getComputedStyle(textarea);
 
-function setScrollPositions(positions: Positions): void {
+	textarea.style.height = String(textarea.scrollHeight + parseFloat(style.borderTopWidth!) + parseFloat(style.borderBottomWidth!)) + 'px';
+
+	// Restore any scrollTop that was lost
 	for (const [element, position] of positions) {
-		element.scrollTop = position;
+		if (position && element.scrollTop !== position) {
+			element.scrollTop = position;
+		}
 	}
 }
 
