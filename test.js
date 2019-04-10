eb2887b1e89da699fdc4c708a25373ae5a1ab0f2
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-10 08:16:43 +0700

    Add tests

---
+++ test.js
@@ -0,0 +1,17 @@
+import test from 'tape';
+import fitTextarea from '.';
+
+document.body.innerHTML = `
+	<textarea>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla placerat enim risus, at convallis odio venenatis non. Aenean non augue nibh. Phasellus aliquam tempus orci, quis viverra odio placerat ac. Duis tristique neque lectus, vitae tincidunt massa dapibus sit amet. Duis vitae pulvinar mi. Suspendisse tincidunt risus quis commodo finibus. Vestibulum suscipit diam metus, eget suscipit eros mollis sed. Vivamus cursus augue turpis, sit amet luctus lectus vehicula sit amet. Duis ut tincidunt mi. Maecenas iaculis eget lorem vestibulum suscipit.
+`;
+
+const textarea = document.querySelector('textarea');
+const getHeight = el = el.getBoundingClientRect().height;
+
+test('fit content once', t => {
+	const initialHeight = getHeight(textarea);
+	fitTextarea(textarea);
+	t.true(initialHeight < getHeight(textarea));
+	t.true(elem.clientHeight >= elem.scrollHeight);
+});
