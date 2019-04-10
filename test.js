bb8fe5a4d620c8488bea812288cb1de53f31268d
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-10 16:46:17 +0700

    add test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const fitTextarea = require('.');
 
-const getField = (repeat = 50) = {
+const getField = (repeat = 50) => {
 const field = document.createElement('textarea');
 field.textContent = `Lorem ipsum dolor sit amet`.repeat(repeat);
 document.body.append(field);
@@ -17,4 +17,13 @@ test('fit content once', t => {
 	fitTextarea(textarea);
 	t.true(initialHeight < getHeight(textarea));
 	t.true(textarea.clientHeight >= textarea.scrollHeight);
+});
+
+test('fit content once + undo when empty', t => {
+	const textarea = getField();
+	const initialHeight = getHeight(textarea);
+	fitTextarea(textarea);
+	textarea.value = '';
+	fitTextarea(textarea);
+	t.equal(initialHeight, getHeight(textarea));
 });
